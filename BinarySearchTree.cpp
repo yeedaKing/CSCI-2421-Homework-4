2b696a1d8ab79c00a0a9583746303975afec4204
yeedaKing <143111017+yeedaKing@users.noreply.github.com>
2024-03-19 05:05:45 +0000

    progress

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,11 +1,11 @@
-#include BinarySearchTree.h
+#include "BinarySearchTree.h"
 
 BinarySearchTree::BinarySearchTree(vector<int> vals) {
 
 }
 
-~BinarySearchTree::BinarySearchTree() {
-    dfs();
+BinarySearchTree::~BinarySearchTree() {
+    dfs(root);
 }
 
 void BinarySearchTree::dfs(TreeNode* curr) {
@@ -39,18 +39,52 @@ TreeNode* BinarySearchTree::findNode(int val) {
 }
 
 void BinarySearchTree::preOrderTraversal() {
-    
+    stack<TreeNode*> s;
+    TreeNode* current = root;
+    while (!s.empty() || current) {
+        while (current) {
+            s.push(current);
+            cout << current->getVal() << ' ';
+            current = current->getLeft();
+        }
+        current = s.top()->getRight();
+        s.pop();
+    }
+    cout << endl;
 }
 
 void BinarySearchTree::inOrderTraversal() {
-
+    stack<TreeNode*> s;
+    TreeNode* current = root;
+    while (!s.empty() || current) {
+        while (current) {
+            s.push(current);
+            current = current->getLeft();
+        }
+        current = s.top();
+        cout << current->getVal() << ' ';
+        current = current->getRight();
+        s.pop();
+    }
+    cout << endl;
 }
 
 void BinarySearchTree::postOrderTraversal() {
-
+    helper(root);
+    cout << endl;
+}
+void BinarySearchTree::helper(TreeNode* node) {
+    if (!node) {
+        return;
+    }
+    helper(node->getLeft());
+    helper(node->getRight());
+    cout << node->getVal() << ' ';
 }
 
-int BinarySearchTree::findHeight() {
-    TreeNode* curr = root;
-    
+int BinarySearchTree::findHeight(TreeNode* node) {
+    if (!node) {
+        return 0;
+    }
+    return 1+max(findHeight(node->getLeft()), findHeight(node->getRight()));
 }
